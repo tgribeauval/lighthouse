f24230654554caf8e165aef72000001431b97fce
tgribeauval <36380615+tgribeauval@users.noreply.github.com>
2018-06-05 14:13:12 -0400

    Create average.js

---
+++ average.js
@@ -0,0 +1,27 @@
+function average(list) {
+  var sum = 0;
+
+
+  for (var num of list) {
+    sum += num;
+  }
+
+  return sum / list.length;
+}
+
+console.log(average([3, 5, 7]));
+
+
+
+
+
+
+
+var input = process.argv[2];
+
+if (input)
+  console.log(reverse(input));
+
+function reverse(original) {
+  return original.split('').reverse().join('');
+}
