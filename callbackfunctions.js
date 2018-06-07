42d9d3fc92e326831ee76fc95750b31c2c557df1
tgribeauval <36380615+tgribeauval@users.noreply.github.com>
2018-06-07 12:21:49 -0400

    Calling Back Functions.

---
+++ callbackfunctions.js
@@ -0,0 +1,57 @@
+// // The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(index) {
+
+      console.log("Found him at index:", index);
+
+
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
+
+
+
+
+
+// // The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found();   // execute callback
+    }
+  }
+}
+
+function actionWhenFound() {
+  console.log("Found him!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  var result = arr.forEach((z, i, arr) => {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  });
+};
+
+function actionWhenFound() {
+  console.log("Found him!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
